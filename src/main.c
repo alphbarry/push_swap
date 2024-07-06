a9f0449c959bf2ba91d3c6bdea234e775f9978ab
Alpha Omar Barry <alphbarr@car10s1.42barcelona.com>
2024-07-06 21:33:10 +0200

    cambio de algoritmo

--- src/main.c
+++ src/main.c
@@ -6,50 +6,21 @@
 /*   By: alphbarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 18:29:46 by alphbarr          #+#    #+#             */
-/*   Updated: 2024/07/05 20:54:05 by alphbarr         ###   ########.fr       */
+/*   Updated: 2024/07/06 21:32:41 by alphbarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "push_swap.h"
-
-void	get_numbers(char *av, t_stack **s_a)
-{
-	char	**param;
-	int		n;
-	int		i;
-
-	param = ft_split(av, ' ');
-	i = 0;
-	while (param[i])
-	{
-		if (input_is_correct(param[i]))
-		{
-			n = ft_atoi(param[i]);
-			if (n > INT_MAX || n < INT_MIN)
-				ft_error(s_a, NULL);
-			add_stack(s_a, new_stack(n));
-		}
-		else
-			ft_error(NULL, NULL);
-		free(param[i]);
-		i++;
-	}
-	free(param);
-}
+#include "../inc/push_swap.h"
 
 int	main(int ac, char **av)
 {
 	t_stack	*s_a;
 	t_stack	*s_b;
-	int		s_size;
-	int		i;
+	char	**args;
 
-	i = 1;
-	s_a = NULL;
-	s_b = NULL;
-	while (i < ac)
+	if (ac < 1)
 	{
-		get_numbers(av[i], &s_a);
-		i++;
+		args = NULL;
+		return (0);
 	}
-	if (is_duplicate)
+	else if (ac == 2)
 }
